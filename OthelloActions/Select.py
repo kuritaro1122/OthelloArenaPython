ade03255d5e1725b869a4f9920e2870dddbb2cd7
KuriTaro <s.cream4956@gmail.com>
2023-06-02 19:17:38 +0900

    del Select.py

--- OthelloActions/Select.py
+++ OthelloActions/Select.py
@@ -1,9 +1,9 @@
-def getAction(board,moves):
-    for i, m in enumerate(moves):
-        print('{0} : {1}'.format(i, m))
-    index = -1
-    while index < 0 or index >= len(moves):
-        message = input('手を選んでください({0}~{1}) : '.format(0, len(moves)))
-        if message.isdecimal:
-            index = int(message)
-    return moves[index]
+#def getAction(board,moves):
+#    for i, m in enumerate(moves):
+#        print('{0} : {1}'.format(i, m))
+#    index = -1
+#    while index < 0 or index >= len(moves):
+#        message = input('手を選んでください({0}~{1}) : '.format(0, len(moves)))
+#        if message.isdecimal:
+#            index = int(message)
+#    return moves[index]
